d31e522410485bdfff1732ab8e4966d4cfdc8120
SONALI PATEL <patelsonali123@yahoo.com>
2018-04-12 15:23:34 -0400

    Presenting LoginVC from App Delegate and allowing login to firebase through email is done

--- BreakpointApp/BreakpointApp/Controller/AuthViewController.swift
+++ BreakpointApp/BreakpointApp/Controller/AuthViewController.swift
@@ -21,15 +21,17 @@ class AuthViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+   
+  
+    @IBAction func signInWithEmailBtnWasPressed(_ sender: Any) {
+        let loginVC = storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+        present(loginVC!, animated: true, completion: nil)
     }
-    */
-
+    
+   
+    @IBAction func googleSignInBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func facebookSignInBtnWasPressed(_ sender: Any) {
+    }
 }
--- BreakpointApp/BreakpointApp/Controller/LoginViewController.swift
+++ BreakpointApp/BreakpointApp/Controller/LoginViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var emailField: InsetTextField!
+    
+    @IBOutlet weak var passwordField: InsetTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        emailField.delegate = self
+        passwordField.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +24,39 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func signInBtnWasPressed(_ sender: Any) {
+        if emailField.text != nil && passwordField.text != nil {
+            AuthService.instance.loginUser(withEmail: emailField.text!, andPassword: passwordField.text!, loginComplete: { (success, loginError) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print(String(describing: loginError?.localizedDescription))
+                }
+                
+                AuthService.instance.registerUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, userCreationComplete: { (success, registrationError) in
+                    if success {
+                    AuthService.instance.loginUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, loginComplete: { (success, nil) in
+                            print("Successfully registered user")
+                        self.dismiss(animated: true, completion: nil)
+                        })
+                    } else {
+                        print(String(describing: registrationError?.localizedDescription))
+                    }
+                })
+                
+            })
+        }
     }
-    */
+    
 
+   
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
 }
