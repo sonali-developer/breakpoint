18f433a2814d4ed2d6e832716a2a56b102911d6b
SONALI PATEL <patelsonali123@yahoo.com>
2018-04-18 02:32:17 -0400

    Done with FeedCell design in IB and code write up

---
+++ BreakpointApp/BreakpointApp/View/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  BreakpointApp
+//
+//  Created by Sonali Patel on 4/18/18.
+//  Copyright © 2018 Sonali Patel. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
