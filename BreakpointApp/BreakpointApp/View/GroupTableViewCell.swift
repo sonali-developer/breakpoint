cedd4f19316f18a390b753a60a750013f9983d9b
SONALI PATEL <patelsonali123@yahoo.com>
2018-04-18 09:49:03 -0400

    Design of GroupsTableView Cell done

---
+++ BreakpointApp/BreakpointApp/View/GroupTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupTableViewCell.swift
+//  BreakpointApp
+//
+//  Created by Sonali Patel on 4/18/18.
+//  Copyright © 2018 Sonali Patel. All rights reserved.
+//
+
+import UIKit
+
+class GroupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
